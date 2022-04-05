922aaa53d756c6abd7747c783cdc2c6ad1427ed2
Natalia Teixeira <nataliat1026@gmail.com>
2022-04-05 19:47:18 -0400

    commit tree 51347ae3dc8832bdaa80a3cc1915905069cf1f96

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// code your solution here
+function saturdayFun(activity) {
+    if (typeof activity !== 'undefined') {
+        return `This Saturday, I want to ${activity}!`;
+    } else if (typeof activity === 'undefined') {
+        const activity = 'roller-skate';
+        return `This Saturday, I want to ${activity}!`;
+    }
+}
+
+function mondayWork(workSpot) {
+    if (workSpot === 'work from home') {
+        return `This Monday, I will ${workSpot}.`;
+    } else if (typeof workSpot === 'undefined') {
+        const workSpot = 'go to the office';
+        return `This Monday, I will ${workSpot}.`;
+    }
+}
+
+function wrapAdjective(visualFlair = '*') {
+    return function(trait = 'special') {
+        return `You are ${visualFlair}${trait}${visualFlair}!`;
+    }
+}
+
+//wrapAdjective('!!')('incredibly lucky if you get this right');
